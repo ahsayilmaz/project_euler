0543e5c25ac224120b5a02631efcf68dda99839c
ahsayilmaz <ahsayilmaz@gmail.com>
2025-03-30 23:32:35 +0300

    a

---
+++ project_euler/Multiples_of_3_or_5.cs
@@ -0,0 +1,19 @@
+﻿namespace project_euler;
+
+public class Multiples_of_3_or_5
+{
+    public static int Main(int n)
+    {
+        int sum = 0;
+        int number = 0;
+        while (number<n)
+        {
+            if (number % 3 == 0 || number % 5 == 0)
+            {
+                sum += number;
+            }
+            number++;
+        }
+        return sum;
+    }
+}
---
+++ project_euler/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using project_euler;
+
+Console.WriteLine(Multiples_of_3_or_5.Main(1000));
