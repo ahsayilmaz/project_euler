5598a22f963a703aa1084f471b97bfbddf668190
ahsayilmaz <ahsayilmaz@gmail.com>
2025-03-31 10:23:41 +0300

    Largest_Prime_Factor.cs update

--- project_euler/Largest_Prime_Factor.cs
+++ project_euler/Largest_Prime_Factor.cs
@@ -1,29 +1,12 @@
 ﻿namespace project_euler;
 
-public class Largest_Prime_Factor
+public class LargestPrimeFactor
 {
     public static long Main(long n)
     {
         long largest=0;
-
-        for (int i = 0; i < 7; i++)
-        {
-            if ((n - i) % 2 == 0 || (n - i) % 3 == 0)
-            {
-                continue;
-            }
-            for (int j = 5; j*j < (n - i); j+=6)
-            {
-                if ((n - i) % j == 0 || (n-i) % (j + 2) == 0)
-                {
-                    continue;
-                }
-            }
-            largest = (n - i);
-            break;
-        }
         
-        for (long i = 5; i <= n/2; i+=6)
+        /*for (long i = 5; i <= n/2; i+=6)
         {
             if (n % i == 0)
             {
@@ -32,7 +15,34 @@ public class Largest_Prime_Factor
             {
                 largest = (i + 2);
             }
+        }*/
+
+        for (long i = 2; i * i <= n; i++)
+        {
+            if (n % i != 0) continue;
+            var tempIndex=i;
+            var tempPrime= (n / i);
+            if (tempPrime % 2 == 0 || tempPrime % 3 == 0)
+                continue;
+            for (long j = 5; j * j <= tempPrime; j += 6)
+            {
+                if (tempPrime % i == 0 || tempPrime % (i + 2) == 0)
+                {
+                    tempIndex++;
+                    break;
+                }
+            }
+
+            if (i == tempIndex)
+            {
+                largest = tempPrime;
+            }
+            else
+            {
+                tempIndex--;
+            }
         }
+
         return largest;
     }
 }
--- project_euler/Program.cs
+++ project_euler/Program.cs
@@ -4,4 +4,4 @@ using project_euler;
 
 //Console.WriteLine(Multiples_of_3_or_5.Main(1000));
 //Console.WriteLine(Even_Fibonacci_Numbers.Main(4000000));
-Console.WriteLine(Largest_Prime_Factor.Main(600851475143));
+Console.WriteLine(LargestPrimeFactor.Main(600851475143));
