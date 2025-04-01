16c5cdb5cd546d6c7695779c28621bf5ce2a8351
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-01 13:04:41 +0300

    LargestPalindromeProduct.cs update

--- project_euler/LargestPalindromeProduct.cs
+++ project_euler/LargestPalindromeProduct.cs
@@ -30,69 +30,7 @@ public class LargestPalindromeProduct
             }
             return reversedNum == number;
         }
-        
+
         return largestPalindrome;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-            /*int largestPalindrome = 0;
-
-            for (int i = 999; i >= 100; i--)
-            {
-                for (int j = i; j >= 100; j--)
-                {
-                    int product = i * j;
-                    if (IsPalindrome(product) && product > largestPalindrome)
-                    {
-                        largestPalindrome = product;
-                    }
-                }
-            }
-            bool IsPalindrome(int number)
-            {
-                int originalNumber = number;
-                int reversedNumber = 0;
-
-                while (number > 0)
-                {
-                    int digit = number % 10;
-                    reversedNumber = reversedNumber * 10 + digit;
-                    number /= 10;
-                }
-
-                return originalNumber == reversedNumber;
-            }
-
-            return largestPalindrome;*/
     }
 }
