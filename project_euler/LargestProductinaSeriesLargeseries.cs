7a0d3116b010344b9284520d098f47a6275a67f2
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-02 23:35:46 +0300

    largestProductinSeries

---
+++ project_euler/LargestProductinaSeriesLargeseries.cs
@@ -0,0 +1,47 @@
+﻿namespace project_euler;
+
+public class LargestProductinaSeriesLargeseries
+{
+    public static int LargestProductinaSeries(string n, int adjacent)
+    {
+        
+        int[] digits = new int[adjacent];
+        int largestProduct = 1;
+        for (int i = 0; i < adjacent; i++)
+        {
+            if (i < n.Length)
+            {
+                digits[i] = n[n.Length - 1 - i] - '0';
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+            largestProduct *= digits[i];
+            n.Remove(n.Length - 1, 1);
+        }
+
+        for (int i = 0; i < n.Length-adjacent; i++)
+        { 
+            int remainder = i % adjacent;
+            if (remainder < n.Length)
+            {
+                digits[remainder] = n[n.Length - 1 - remainder] - '0';
+            }
+            else
+            {
+                digits[remainder] = 0;
+            }
+            largestProduct *= digits[remainder];
+            n.Remove(n.Length - 1 - remainder, 1);
+            int tempProduct=1;
+            for (int j = 0; j < digits.Length; j++)
+            {
+                tempProduct *= digits[j];
+            }
+            if (largestProduct < tempProduct) largestProduct = tempProduct;
+        }
+        
+        return largestProduct;
+    }
+}
---
+++ project_euler/LargestProductinaSeriesSmallseries.cs
@@ -0,0 +1,34 @@
+﻿namespace project_euler;
+
+public class LargestProductinaSeriesSmallseries
+{
+    public static int Main(string n)
+    {
+        int number = int.Parse(n);
+        Console.WriteLine(number);
+        int largestProduct = 1; 
+        int[] digits = new int[4];
+        for (int i = 0; i <4; i++)
+        {
+            largestProduct *= number%10;
+            digits[i] = number%10;
+            number /= 10;
+        }
+
+        for (int i = 0; i < n.Length - 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                digits[j]=number%10;
+                number /= 10;
+            }
+            int tempProduct=digits[0]*digits[1]*digits[2]+digits[3];
+
+            if (largestProduct == tempProduct)
+            {
+                largestProduct = tempProduct;
+            }
+        }
+        return largestProduct;
+    }
+}
