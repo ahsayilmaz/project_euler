d58f252a1c2438e757be16cc78e4454354de7a28
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-03 21:27:24 +0300

    SpecialPythagoreanTriplet.cs solution

---
+++ project_euler/SpecialPythagoreanTriplet.cs
@@ -0,0 +1,15 @@
+﻿namespace project_euler;
+
+public class SpecialPythagoreanTriplet
+{
+    public static int Main(int num)
+    {
+        for (int a = 1; a < num; a++)
+            for (int b = a + 1; b < num; b++)
+            {
+                int c=num-a-b;
+                if (c>b && a * a + b * b == c * c) return a * b * c;
+            }
+        return 0;
+    }
+}
