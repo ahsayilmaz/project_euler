483885afa054a7da39e6776f83477bf45aa4b421
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-05 23:38:44 +0300

    LongestCollatzSequence.cs and LargeSum.cs start

---
+++ project_euler/LargeSum.cs
@@ -0,0 +1,9 @@
+﻿namespace project_euler;
+
+public class LargeSum
+{
+    public static int Main(string n)
+    {
+        return 0;
+    }
+}
---
+++ project_euler/LongestCollatzSequence.cs
@@ -0,0 +1,25 @@
+﻿namespace project_euler;
+
+public class LongestCollatzSequence
+{
+    public static int Main(int n)
+    {
+        
+        int largestSequence = 0;
+        for (int i = n-1; i > 1; i--)
+        {
+            int sequence = 0;
+            int temp = i;
+            while (temp!=1)
+            {
+                if(temp%2==0)
+                    temp = temp/2;
+                else
+                    temp = temp*3+1;
+                sequence++;
+            }
+            if (sequence > largestSequence) largestSequence = sequence;
+        }
+        return largestSequence;
+    }
+}
