0c46c570dd7d727e5736d93552a5d0b3a3911d64
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-06 23:53:14 +0300

    LongestCollatzSequence.cs finished

--- project_euler/LongestCollatzSequence.cs
+++ project_euler/LongestCollatzSequence.cs
@@ -4,12 +4,12 @@ public class LongestCollatzSequence
 {
     public static int Main(int n)
     {
-        
         int largestSequence = 0;
+        int number = 0;
         for (int i = n-1; i > 1; i--)
         {
-            int sequence = 0;
-            int temp = i;
+            int sequence = 1;
+            long temp = i;
             while (temp!=1)
             {
                 if(temp%2==0)
@@ -18,8 +18,13 @@ public class LongestCollatzSequence
                     temp = temp*3+1;
                 sequence++;
             }
-            if (sequence > largestSequence) largestSequence = sequence;
+
+            if (sequence > largestSequence)
+            {
+                largestSequence = sequence;
+                number = i;
+            }
         }
-        return largestSequence;
+        return number;
     }
 }
