16a9d118b0e1e3fa65ad05b542a4ea405867a6d9
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-09 22:18:21 +0300

    LatticePaths.cs

---
+++ project_euler/LatticePaths.cs
@@ -0,0 +1,19 @@
+﻿namespace project_euler;
+
+public class LatticePaths
+{
+    public static long faktöriyel(int num)
+    {
+        if (num < 0)
+            return -1;
+        if (num == 0)
+            return 1;
+        long faktöriyel = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            faktöriyel *= i;
+        }
+
+        return faktöriyel;
+    }
+}
