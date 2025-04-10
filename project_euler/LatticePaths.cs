4781ec91c3a54129ab98b645ffe0dd3f2db20ede
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-10 22:02:29 +0300

    LatticePaths.cs continue

--- project_euler/LatticePaths.cs
+++ project_euler/LatticePaths.cs
@@ -16,4 +16,28 @@ public class LatticePaths
 
         return faktöriyel;
     }
+
+    public static long kombinasyon(int num, int k)
+    {
+        if (k < 0 || k > n)
+        {
+            return -1;
+        }
+
+        if (k==0||k==n)
+        {
+            return 1;
+        }
+
+        if (k > num / 2)
+        {
+            k = num - k;
+        }
+        long sonuc = 0;
+        for (int i = 1; i <= k; i++)
+        {
+            sonuc = sonuc * (num-i+1)/i;
+        }
+        return sonuc;
+    }
 }
