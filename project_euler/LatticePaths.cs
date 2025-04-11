2ea9d76b6586d99bff8dd115f6a14c05207f6880
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-11 22:39:02 +0300

    LatticePaths.cs

--- project_euler/LatticePaths.cs
+++ project_euler/LatticePaths.cs
@@ -19,12 +19,12 @@ public class LatticePaths
 
     public static long kombinasyon(int num, int k)
     {
-        if (k < 0 || k > n)
+        if (k < 0 || k > num)
         {
             return -1;
         }
 
-        if (k==0||k==n)
+        if (k==0||k==num)
         {
             return 1;
         }
@@ -33,11 +33,21 @@ public class LatticePaths
         {
             k = num - k;
         }
-        long sonuc = 0;
+        long sonuc = 1;
         for (int i = 1; i <= k; i++)
         {
-            sonuc = sonuc * (num-i+1)/i;
+            sonuc *= (num-i+1)/i;
         }
         return sonuc;
+    }
+
+    public static long kafesYolusayisi(int satır, int sütun)
+    {
+        if (satır <= 0 || sütun <= 0)
+        {
+            return -1;
+        }
+
+        return kombinasyon(satır + sütun - 2, satır - 1);
     }
 }
