0e2fc99514c60619705ab9f4cac23ce968b78652
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-12 16:28:36 +0300

    LatticePaths.cs

--- project_euler/LatticePaths.cs
+++ project_euler/LatticePaths.cs
@@ -2,52 +2,21 @@
 
 public class LatticePaths
 {
-    public static long faktöriyel(int num)
-    {
-        if (num < 0)
-            return -1;
-        if (num == 0)
-            return 1;
-        long faktöriyel = 1;
-        for (int i = 1; i <= num; i++)
-        {
-            faktöriyel *= i;
-        }
 
-        return faktöriyel;
-    }
-
-    public static long kombinasyon(int num, int k)
-    {
-        if (k < 0 || k > num)
-        {
-            return -1;
-        }
-
-        if (k==0||k==num)
-        {
-            return 1;
-        }
-
-        if (k > num / 2)
-        {
-            k = num - k;
-        }
-        long sonuc = 1;
-        for (int i = 1; i <= k; i++)
-        {
-            sonuc *= (num-i+1)/i;
-        }
-        return sonuc;
-    }
-
-    public static long kafesYolusayisi(int satır, int sütun)
+    public static double kafesYolusayisi(int satır, int sütun)
     {
         if (satır <= 0 || sütun <= 0)
         {
             return -1;
         }
+        if (satır <= 0 || sütun<=0)
+            return 0;
+        double permütasyon = 1;
+        for (double i = satır+1, j=1; i <= satır+sütun; i++, j++)
+        {
+            permütasyon *= (i / j);
+        }
 
-        return kombinasyon(satır + sütun - 2, satır - 1);
+        return permütasyon;
     }
 }
