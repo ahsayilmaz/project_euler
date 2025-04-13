df299be34d1c23253d85088b7d7edc8caa2d06dc
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-13 21:34:34 +0300

    LatticePaths.cs fixed and finished

--- project_euler/LatticePaths.cs
+++ project_euler/LatticePaths.cs
@@ -3,7 +3,7 @@
 public class LatticePaths
 {
 
-    public static double kafesYolusayisi(int satır, int sütun)
+    public static long kafesYolusayisi(int satır, int sütun)
     {
         if (satır <= 0 || sütun <= 0)
         {
@@ -11,12 +11,24 @@ public class LatticePaths
         }
         if (satır <= 0 || sütun<=0)
             return 0;
-        double permütasyon = 1;
-        for (double i = satır+1, j=1; i <= satır+sütun; i++, j++)
+        long permütasyon = 1;
+        long remainder = 1;
+        for (int i = satır+1, j=1; i <= satır+sütun; i++, j++)
         {
-            permütasyon *= (i / j);
+            if (i%j==0)
+                permütasyon *= (i / j);
+            else if (permütasyon % j == 0)
+            {
+                permütasyon = permütasyon / j;
+                permütasyon*= i;
+            }
+            else
+            {
+                remainder *= j;
+                permütasyon *= i;
+            }
         }
 
-        return permütasyon;
+        return permütasyon/remainder;
     }
 }
