1f8897a8da085a322e4de50115fac6ce80f9d3ba
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-19 22:49:18 +0300

    maximumPathSum.cs start

---
+++ project_euler/maximumPathSum.cs
@@ -0,0 +1,16 @@
+﻿namespace project_euler;
+
+public class maximumPathSum
+{
+    public static int Main(string sayi)
+    {
+        string[] inputs = sayi.Split('\n');
+        int[][] triangle = new int[inputs.Length][];
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            triangle[i] = inputs[i].Split(' ').Select(int.Parse).ToArray();
+        }
+        
+        return 0;
+    }
+}
