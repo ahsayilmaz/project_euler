a6b4ccc4d1502d7f3c21ccf8b3c21e58101bd615
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-22 23:09:43 +0300

    countingSundays.cs start

---
+++ project_euler/countingSundays.cs
@@ -0,0 +1,15 @@
+﻿namespace project_euler;
+
+public class countingSundays
+{
+    public static int Main(int start, int end)
+    {
+        int sundays = 0;
+        int whatDay = 1;//1:monday, 7:sunday
+        for (int i = start; i <= end; i++)
+        {
+            whatDay+=31 % 7 - 1;
+        }
+        return 0;
+    }
+}
