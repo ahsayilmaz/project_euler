f628ca85bb7d175ecb2ec8baaa63a9da8dd71578
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-04-23 23:38:01 +0300

    Update countingSundays.cs

--- project_euler/countingSundays.cs
+++ project_euler/countingSundays.cs
@@ -5,10 +5,13 @@ public class countingSundays
     public static int Main(int start, int end)
     {
         int sundays = 0;
-        int whatDay = 1;//1:monday, 7:sunday
+        int whatDay = 0;//0:monday, 7:sunday
         for (int i = start; i <= end; i++)
         {
             whatDay+=31 % 7 - 1;
+            if (whatDay%7==6){
+                sundays++
+                }
         }
         return 0;
     }
