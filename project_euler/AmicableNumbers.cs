c0b3c535c3bbca57792f58be2707e1feb084b046
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-25 22:24:26 +0300

    AmicableNumbers.cs start

---
+++ project_euler/AmicableNumbers.cs
@@ -0,0 +1,18 @@
+﻿namespace project_euler;
+
+public class AmicableNumbers
+{
+    public static int Main(int num)
+    {
+        int AmicableSum = 0;
+        
+        for (int i = 2; i < num; i++)
+        {
+            for (int j = 1; j < num; j++)
+            {
+                
+            }
+        }
+        return AmicableSum;
+    }
+}
