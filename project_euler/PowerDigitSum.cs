8e6ae1143953de343fc4c43d032f5665351a0f0d
ahsayilmaz <ahsayilmaz@gmail.com>
2025-04-29 23:13:49 +0300

    PowerDigitSum.cs continue

--- project_euler/PowerDigitSum.cs
+++ project_euler/PowerDigitSum.cs
@@ -4,24 +4,31 @@ public interface PowerDigitSum
 {
     public static int GetPowerDigitSum(int power)
     {
-        long powerOf59 = 1024;
-        for (int i = 0; i < 5; i++)//until 2^60
+        long powerOf50 = 1024;
+        for (int i = 0; i < 4; i++)//until 2^60
         {
-            powerOf59 *= 1024;
+            powerOf50 *= 1024;
         }
-        
-        string powerOf63 = (powerOf59*16).ToString();//2^63
-        for (int i = 64; i <= power; i++)//keep on calculating the value of powers of 2 until 1000
+        string until1000 = (powerOf50*1024).ToString();//2^63
+        for (int i = 0; i <16; i++)//keep on calculating the value of powers of 2 until 1000
         {
             string nextPower = "";
             long sum = 0;
-            for (int j = powerOf63.Length; j >=0; j--)
+            for (int j = until1000.Length-1; j >=0; j--)
             {
-                sum += int.Parse(powerOf63[j].ToString())*powerOf59;
+                sum += int.Parse(until1000[j].ToString()) * powerOf50;
+                nextPower = (sum % 10)+nextPower;
+                sum /= 10;
             }
-            
+            Console.WriteLine(sum );
+            until1000 = sum + nextPower;
         }
-        
-        return 0;
+
+        int digitSum = 0;
+        for (int i = until1000.Length-1; i >= 0; i--)
+        {
+            digitSum += int.Parse(until1000[i].ToString());
+        }
+        return digitSum;
     }
 }
