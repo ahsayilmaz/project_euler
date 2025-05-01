17e824d0f61dd2a7bc5886452f7f0d460f7df39a
ahsayilmaz <ahsayilmaz@gmail.com>
2025-05-01 23:36:32 +0300

    1000DigitFibonacciNumber.cs start

---
+++ project_euler/1000DigitFibonacciNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace project_euler;
+
+public class First1000DigitFibonacciNumber {
+    public static int Main(int n)
+    {
+        int First1000Digit=0;
+        long a = 0;
+        long b = 1;
+        
+        for (int i = 1; i < n; i++) 
+        {
+            long nextFib = a + b;
+            a = b;
+            b = nextFib;
+        }
+
+        return First1000Digit;
+    }
+}
