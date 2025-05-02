927ca8496179f428751f089a6dafdf816154fd75
ahsayilmaz <ahsayilmaz@gmail.com>
2025-05-02 20:05:14 +0300

    1000DigitFibonacciNumber.cs continue

--- project_euler/1000DigitFibonacciNumber.cs
+++ project_euler/1000DigitFibonacciNumber.cs
@@ -3,17 +3,31 @@
 public class First1000DigitFibonacciNumber {
     public static int Main(int n)
     {
-        int First1000Digit=0;
         long a = 0;
         long b = 1;
         
-        for (int i = 1; i < n; i++) 
+        while (b.ToString().Length < 19 ) 
         {
             long nextFib = a + b;
             a = b;
             b = nextFib;
         }
 
-        return First1000Digit;
+        string aVeryLong = a.ToString();
+        string bVeryLong = b.ToString();
+
+        while (bVeryLong.ToString().Length < 1000)
+        {
+            int length = aVeryLong.Length;
+            string nextFib ="";
+            int sum = 0;
+            for (int i = 0; i<length ; i+=17)
+            {
+                
+            }
+            
+        }
+
+        return 0;
     }
 }
