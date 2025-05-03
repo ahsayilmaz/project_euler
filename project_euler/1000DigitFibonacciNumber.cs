188bfda129c8cbe00ed124abbfd0fa155b17c735
ahsayilmaz <ahsayilmaz@gmail.com>
2025-05-03 23:22:27 +0300

    1000DigitFibonacciNumber.cs continue

--- project_euler/1000DigitFibonacciNumber.cs
+++ project_euler/1000DigitFibonacciNumber.cs
@@ -3,6 +3,7 @@
 public class First1000DigitFibonacciNumber {
     public static int Main(int n)
     {
+        int count = 1;
         long a = 0;
         long b = 1;
         
@@ -11,23 +12,32 @@ public class First1000DigitFibonacciNumber {
             long nextFib = a + b;
             a = b;
             b = nextFib;
+            count++;
         }
 
         string aVeryLong = a.ToString();
         string bVeryLong = b.ToString();
+        long modVeryLong = 100000000000000000;
 
-        while (bVeryLong.ToString().Length < 1000)
+        while (bVeryLong.Length < 1000)
         {
-            int length = aVeryLong.Length;
             string nextFib ="";
-            int sum = 0;
-            for (int i = 0; i<length ; i+=17)
+            long sum = 0;
+            for (int i = aVeryLong.Length-1; i>=17; i-=17)
             {
-                
+                sum += Convert.ToInt64(aVeryLong.Substring(i-17, 17));
+                sum += Convert.ToInt64(bVeryLong.Substring(i-17, 17));
+                nextFib = (sum%modVeryLong)+nextFib;
+                sum /= modVeryLong;
             }
-            
+            nextFib = sum + nextFib;
+            if(bVeryLong.Length > aVeryLong.Length)
+                nextFib = bVeryLong.Substring(bVeryLong.Length-aVeryLong.Length)+nextFib;
+            aVeryLong = bVeryLong;
+            bVeryLong = nextFib;
+            count++;
         }
 
-        return 0;
+        return count;
     }
 }
