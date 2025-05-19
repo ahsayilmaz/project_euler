d08b109d32c752e1b1d27c73a0761413ac381da2
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-05-19 21:39:36 +0300

    modified:   project_euler/NonAbundantsums.cs

--- project_euler/NonAbundantsums.cs
+++ project_euler/NonAbundantsums.cs
@@ -13,44 +13,46 @@ public class NonAbundantsums
                 if (i % j == 0)
                 {
                     sumOfDivisors += j;
-                    if (i % (i / j) == 0)
+                    if (j * j != i)
                     {
                         sumOfDivisors += i / j;
                     }
                 }
             }
+
             if (sumOfDivisors > i)
             {
                 abundantNumbers.Add(i);
             }
         }
 
-        bool[] canBeWritten = new bool[n + 1];
+        bool[] isSumOfTwoAbundants = new bool[n + 1];
+
         for (int i = 0; i < abundantNumbers.Count; i++)
         {
             for (int j = i; j < abundantNumbers.Count; j++)
             {
-                int sum = abundantNumbers[i] + abundantNumbers[j];
-                if (sum <= n)
+                int currentSum = abundantNumbers[i] + abundantNumbers[j];
+                if (currentSum <= n)
                 {
-                    canBeWritten[sum] = true;
+                    isSumOfTwoAbundants[currentSum] = true;
                 }
                 else
                 {
-                    break;
+                   break;
                 }
             }
         }
 
-        int totalSum = 0;
-        for (int i = 1; i <= n; i++)
+        int finalSumOfNonAbundantSums = 0;
+        for (int k = 1; k <= n; k++)
         {
-            if (!canBeWritten[i])
+            if (!isSumOfTwoAbundants[k])
             {
-                totalSum += i;
+                finalSumOfNonAbundantSums += k;
             }
         }
 
-        return totalSum;
+        return finalSumOfNonAbundantSums;
     }
 }
