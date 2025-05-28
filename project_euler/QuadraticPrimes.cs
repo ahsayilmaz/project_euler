47f2e337fb41590160729a740be8e60f7b9b05b6
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-05-28 23:22:05 +0300

    modified:   project_euler/QuadraticPrimes.cs

--- project_euler/QuadraticPrimes.cs
+++ project_euler/QuadraticPrimes.cs
@@ -4,7 +4,15 @@ public static class QuadraticPrimes
 {
     public static int Main(int n)
     {
-        return 0;
-        
+        int maxPrimes = 0;
+        int product = 0;
+
+        for (int i = -n + 1; i < n; i++)
+        {
+            for (int j = -n; j < n; j++)
+            {
+            }
+        }
+        return product;
     }
 }
