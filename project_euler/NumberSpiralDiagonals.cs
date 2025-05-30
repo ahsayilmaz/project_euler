511a0c94c9bca1907ef63fc1eecf88678527af6b
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-05-30 23:23:35 +0300

    modified:   project_euler/NumberSpiralDiagonals.cs

--- project_euler/NumberSpiralDiagonals.cs
+++ project_euler/NumberSpiralDiagonals.cs
@@ -5,7 +5,12 @@ public static class NumberSpiralDiagonals
     public static long Main(int n)
     {
         int sum = 1;
-
+        int currentNum = 1;
+        int step = 2;
+        while (currentNum < n * n)
+        {
+            
+        }
         return sum;
     }
 }
