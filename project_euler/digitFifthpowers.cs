08d79c8713d3796b55ece85faf5576ca2e2273de
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-06-05 23:31:22 +0300

    digitFifthpowers.cs

---
+++ project_euler/digitFifthpowers.cs
@@ -0,0 +1,27 @@
+﻿namespace project_euler;
+
+public class DigitFifthpowers
+{
+    public static int Main(int n)
+    {
+        int UstSinir = 360000;
+        int TotalSum = 0;
+        for (int i = 2; i < UstSinir; i++)
+        {
+            double sum = 0;
+            int temp = i;
+            while (0 < i)
+            {
+                sum += Math.Pow((i % 10),n);
+                i /= 10;
+            }
+
+            if (sum == temp)
+            {
+                TotalSum += temp;
+            }
+        }
+        
+        return TotalSum;
+    }
+}
