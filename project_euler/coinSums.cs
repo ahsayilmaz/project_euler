ce4580b9e4623a37dd4f30b031c80b9cb24f2f39
A <103384386+ahsayilmaz@users.noreply.github.com>
2025-06-08 22:32:22 +0300

    coinSums.cs

---
+++ project_euler/coinSums.cs
@@ -0,0 +1,28 @@
+﻿namespace project_euler;
+
+public class coinSums
+{
+    public static int Main(int n)
+    {
+        int limit = n * 100;
+        int pound = 100;
+        int howManyWays= 0;
+        for (int i = 0; i <= limit; i+=pound)
+        {
+            for (int j = 0; j <= limit-i; j += 50)
+            {
+                for (int k = 0; k <= limit - j-i; k += 20)
+                {
+                    for (int l = 0; l <= limit-i-j-k; l+=5)
+                    {
+                        for (int m = 0; m <= limit-i-j-k-l; m+=2)
+                        {
+                            howManyWays++;
+                        }
+                    }
+                }
+            }
+        }
+        return howManyWays;
+    }
+}
